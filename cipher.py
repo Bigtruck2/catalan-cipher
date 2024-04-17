f9445332f270d3d4be6e2444513addd1cda4a7f4
Bigtruck2 <bigburrito363@gmail.com>
2024-04-17 12:29:01 -0400

    initial

---
+++ cipher.py
@@ -0,0 +1,33 @@
+def encrypt(x, key):
+  new_str=''
+  onHold=[]
+  for character in range(0,len(key)):
+    if len(x) > character:
+      onHold.insert(0,x[character])
+    if key[character] == '1':
+      continue
+    else:
+      new_str+=onHold[0]
+      onHold.pop(0)
+  return new_str
+
+def decrypt(x, key):
+  onHold=[]
+  filled=[]
+  count=0
+  for character in range(0,len(key)):
+    if character >= len(x):
+      filled=list(set(range(0,len(x)+1)) - set(filled))
+      x = x[count:][::-1]
+      for i in range(0,len(x)):
+        onHold.insert(filled[i],x[i])
+      break
+    elif key[character] == '0':
+      onHold.insert(character,x[count])
+      count+=1
+      filled.append(character)
+  return ''.join(onHold)
+x= "Hello world"
+key = '1110100111100001100100'
+print("cipher text:", encrypt(x, key))
+print("deciphered text:", decrypt(encrypt(x,key),key))
---
+++ key_generator.py
@@ -0,0 +1,15 @@
+import random
+#make list of 10 ones
+x="1"*11
+#for loop to pick 9 spots for 0s
+for i in range(0,10):
+  #generate randome spot until a valid spot is found
+  while True:
+	#pick a place to put a 0
+    index= random.randint(1,len(x)+1)
+    #check if the number of zeros preceding the selected index is less than the number of ones. Additionally, if the index is a spot where there is already a zero
+    if(x[:index].count("0")+1<x[:index-1].count("1")):
+      x=x[:index]+"0"+x[index:]
+      break
+x+='0'
+print(x)
